7c88917327ed8e6255cc0fe1509fb8aca07d9058
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-09-11 19:11:54 +0000

    first data models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Payment, Subscription, Gender
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,9 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Payment, db.session))
+    admin.add_view(ModelView(Subscription, db.session))
+    admin.add_view(ModelView(Gender, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,33 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import  JWTManager, create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+bcrypt = Bcrypt()
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/signup', methods=['POST'])
+def singup_user():
+  try:
+    user_data = request.json
+    existing_user = User.query.filter_by(email=user_data['email']).first()
+    if existing_user:
+      return jsonify({"error": "Este email ya a sido registrado"}), 409
+    encrypted_password = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
+    
+    new_user = User(
+      email = user_data['email'],
+      password = encrypted_password,
+      role = user_data['role']
+    )
+    
+    db.session.add(new_user)
+    db.session.commit() 
+    return jsonify(new_user.serialize()), 201
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500
