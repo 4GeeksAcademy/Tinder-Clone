562d8c89c5f215e1b8fa9e8c0461eac94d43f153
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-09-16 21:34:41 +0000

    Password length fixed

--- src/api/models.py
+++ src/api/models.py
@@ -62,7 +62,7 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(120), nullable = True)
   email = db.Column(db.String(120), unique=True, nullable=True)
-  password = db.Column(db.String(120), nullable=True)
+  password = db.Column(db.String(255), nullable=True)
   country = db.Column(db.String(80), nullable=True)
   age = db.Column(db.String(80), nullable=True)
   gender_id = db.Column(db.Integer, db.ForeignKey('gender.id'), nullable=True)
