1685c349bbcecf4322b8533194c3ed49e74ada17
Frcru <91342407+Frcru@users.noreply.github.com>
2024-09-16 20:30:58 +0000

    solved offcanvas scroll not working when navigating - Frank

--- src/front/js/component/HomeNotLogHeader.jsx
+++ src/front/js/component/HomeNotLogHeader.jsx
@@ -7,22 +7,22 @@ export const HomeNotLogHeader = () => {
     const navigate = useNavigate();
     const onClickHandler = (link) => {
         navigate(`/${link}`)
+    // const offcanvasElement = document.getElementById('offcanvasNavbar');
+    // if (offcanvasElement) {
+    //     const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+    //     if (bsOffcanvas) {
+    //         bsOffcanvas.hide();
+    //     }
+    // }
     }
-    // const onMouseEnterHandler = (e) => {
-    //     setCursor("pointer")
-    //     console.log(e)
-    //     console.log(cursor)
-    // }
-    // const onMouseLeave = ()=>{
-    //     setCursor("default")
-    // }
+    
     return (
         <div className="me-3 nav-principal" style={{ color: 'transparent', verticalAlign: 'bottom' }} >
             <nav className="navbar navbar-expand-lg sticky-top pt-0" style={{ color: 'transparent' }}>
                 <div className="container-fluid d-flex align-items-center position-relative">
                     <div className="d-flex align-items-end" onClick={() => onClickHandler("")} style={{ cursor: `${cursor}` }}>
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48" >
-                            <radialGradient id="Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1" cx="24.39" cy="40.173" r="38.605" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fe7356"></stop><stop offset="1" stop-color="#fd297c"></stop></radialGradient><path fill="url(#Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1)" d="M17.2,20.187c7.65-2.429,8.864-9.471,7.893-15.786c0,0,0-0.364,0.243-0.243	C32.743,7.802,41,15.452,41,27.23c0,8.743-6.921,16.636-17,16.636c-10.929,0-17-7.65-17-16.757c0-5.464,3.643-10.929,7.893-13.357	c0,0,0.364,0,0.364,0.243c0,1.214,0.486,4.25,1.821,6.071L17.2,20.187z"></path>
+                            <radialGradient id="Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1" cx="24.39" cy="40.173" r="38.605" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#fe7356"></stop><stop offset="1" stopColor="#fd297c"></stop></radialGradient><path fill="url(#Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1)" d="M17.2,20.187c7.65-2.429,8.864-9.471,7.893-15.786c0,0,0-0.364,0.243-0.243	C32.743,7.802,41,15.452,41,27.23c0,8.743-6.921,16.636-17,16.636c-10.929,0-17-7.65-17-16.757c0-5.464,3.643-10.929,7.893-13.357	c0,0,0.364,0,0.364,0.243c0,1.214,0.486,4.25,1.821,6.071L17.2,20.187z"></path>
                         </svg>
                         <p className="navbar-brand brand-resp pb-0 mb-0" style={{ color: 'white', verticalAlign: "bottom", display: "table-cell" }}>Tinder</p>
                     </div>
@@ -33,7 +33,7 @@ export const HomeNotLogHeader = () => {
                         <div className="offcanvas-header">
                             <div className="d-flex align-items-end" onClick={() => onClickHandler("")}>
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
-                                    <radialGradient id="Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1" cx="24.39" cy="40.173" r="38.605" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#fe7356"></stop><stop offset="1" stop-color="#fd297c"></stop></radialGradient><path fill="url(#Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1)" d="M17.2,20.187c7.65-2.429,8.864-9.471,7.893-15.786c0,0,0-0.364,0.243-0.243	C32.743,7.802,41,15.452,41,27.23c0,8.743-6.921,16.636-17,16.636c-10.929,0-17-7.65-17-16.757c0-5.464,3.643-10.929,7.893-13.357	c0,0,0.364,0,0.364,0.243c0,1.214,0.486,4.25,1.821,6.071L17.2,20.187z"></path>
+                                    <radialGradient id="Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1" cx="24.39" cy="40.173" r="38.605" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#fe7356"></stop><stop offset="1" stopColor="#fd297c"></stop></radialGradient><path fill="url(#Tkc2EFCGCAaG3EvLlEqzza_hp54uGDlrEZB_gr1)" d="M17.2,20.187c7.65-2.429,8.864-9.471,7.893-15.786c0,0,0-0.364,0.243-0.243	C32.743,7.802,41,15.452,41,27.23c0,8.743-6.921,16.636-17,16.636c-10.929,0-17-7.65-17-16.757c0-5.464,3.643-10.929,7.893-13.357	c0,0,0.364,0,0.364,0.243c0,1.214,0.486,4.25,1.821,6.071L17.2,20.187z"></path>
                                 </svg>
                                 <p className="navbar-brand brand-resp pb-0 mb-0" onClick={() => onClickHandler("")} style={{ color: 'white',cursor:`${cursor}` }}>Tinder</p>
                             </div>
