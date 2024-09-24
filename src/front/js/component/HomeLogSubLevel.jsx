91543a8dbde63219ae40983f04ea90d40e0f19ac
Frcru <91342407+Frcru@users.noreply.github.com>
2024-09-24 15:48:49 +0000

    incorpored views HomeLoggedSubPremium and HomeLoggedSubVip. * inside ProfileSettings added 2 buttons. *Each button navigates into each view first mentioned. * Responsive design for md and sm (in fact lg xl is same as md)

--- src/front/js/component/HomeLogSubLevel.jsx
+++ src/front/js/component/HomeLogSubLevel.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const HomeLogSubLevel = ({ children }) => {
     return (
-        <div className='p-3' style={{backgroundColor:'#111418', borderRadius:'30px'}}>
+        <div className='p-3' style={{minWidth:'150px'}}>
             {children}
         </div>
     )
 }
 
 export const RowLevel = (props) => {
+    const navigate = useNavigate();
+    const onClickHandler = ()=>{
+        navigate(props.toNavigate)
+    }
     return (
-        <>
-            <h3 style={{color: '#F0F2F4'}}>{props.title}</h3>
+        <div onClick={onClickHandler} style={{backgroundColor:`${props.color}`, borderRadius:'25px', padding:'0.7rem',alignItems:'center'}}>
+            <h3 style={{color: '#F0F2F4', fontSize:'1rem'}}>{props.title}</h3>
             <p style={{color: '#A9BAC8'}}>{props.description}</p>
-        </>
+        </div>
     )
 
 }
