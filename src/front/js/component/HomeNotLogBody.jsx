f709df2c450f4f771d439fa3ed7c94ee6bf37130
Angel Salazar <146801401+AngelSalazarV@users.noreply.github.com>
2024-09-25 21:21:55 +0000

    Home styles

--- src/front/js/component/HomeNotLogBody.jsx
+++ src/front/js/component/HomeNotLogBody.jsx
@@ -4,8 +4,8 @@ import NewPostButton from './NewPostButton'; // Asegúrate de que la ruta sea co
 export const HomeNotLogBody = () => {
     return (
         <div className="container-fluid d-flex flex-column justify-content-center my-auto" style={{minHeight: '87dvh'}}>
-            <div className="container text-center mb-5">
-                <h1 style={{fontSize:'5em', color:'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)'}}>Engancha una cita</h1>
+            <div className="container-fluid text-center mb-5">
+                <h1 style={{fontFamily: 'Montserrat', fontWeight: 'bold' ,fontSize:'9em', color:'white'}}>Swipea a la derecha</h1>
             </div>
             <div className="container text-center">
                 <NewPostButton />
