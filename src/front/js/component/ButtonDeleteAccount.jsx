bb9e3b0b799e6bce219f09e4d7d3edb088855cb3
Frcru <91342407+Frcru@users.noreply.github.com>
2024-09-25 05:38:34 +0000

    some features added

--- src/front/js/component/ButtonDeleteAccount.jsx
+++ src/front/js/component/ButtonDeleteAccount.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { HomeLogSubLevel, RowLevel } from './HomeLogSubLevel.jsx';
-
-
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext.js';
 export const ButtonDeleteAccount = () => {
+    const {actions} = useContext(Context);
+    const deleteAccountHandler = ()=>{
 
+        actions.deleteAccount()
+    }
     return (
         <div className='p-3 '>
-            <div className='container-fluid btn btn-outline-danger btn-lg' style={{backgroundColor: `#111418`, borderRadius: '25px', padding: '0.7rem', alignItems: 'center' }}>
+            <div onClick={deleteAccountHandler} className='container-fluid btn btn-outline-danger btn-lg' style={{backgroundColor: `#111418`, borderRadius: '25px', padding: '0.7rem', alignItems: 'center' }}>
                 Eliminar Cuenta
             </div>
         </div>
